5a80acaf1b08b55796ee73a3984073a7902e5463
shirasss <shirass321@outlook.co.il>
2022-10-23 17:53:45 +0300

    change

---
+++ Stage0/Stage0/Welcome4406.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace stage0
+{
+    partial class Program 
+    { 
+    static void Main(string[] args)
+    {
+            Welcome4406();
+            Welcome7391();
+
+            Console.ReadKey();
+        }
+        static partial void Welcome7391();
+        private static void Welcome4406()
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+
+}
