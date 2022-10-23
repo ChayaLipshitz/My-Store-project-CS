ea2ee9f9da9322ef5d20caaaeb1b72a8671eccda
shirasss <shirass321@outlook.co.il>
2022-10-23 17:06:06 +0300

    stage1

---
+++ stage1/Dal/DO/Product.cs
@@ -0,0 +1,11 @@
+﻿
+namespace DO;
+
+    public struct Product
+    {
+    public int Product_ID { get; set; }
+    public string Product_Name { get; set; }
+    public int Product_Price { get; set; }
+
+}
+
---
+++ stage1/DalList/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace DalList;
+public class Class1
+{
+
+}
