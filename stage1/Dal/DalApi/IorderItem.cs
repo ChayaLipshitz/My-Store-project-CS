b2b7917e7e449fa782704911eac870959ccf331b
shirasss <shirass321@gmail.com>
2022-11-13 16:38:56 +0200

    c

---
+++ stage1/Dal/DalApi/IorderItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dal;
+using Dal.DO;
+
+namespace DalApi
+{
+    public class IorderItem : Icrud<OrderItem>
+    {
+        public int Create(OrderItem obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OrderItem Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(OrderItem obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ stage1/Dal/DalApi/Iproduct.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dal;
+using Dal.DO;
+
+namespace DalApi
+{
+    public interface Iproduct : Icrud<Product>
+    {
+        public int Create(Product obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Product obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
