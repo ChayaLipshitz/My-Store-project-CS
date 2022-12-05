9e1949b7f626de116931994ab5b09a5be965fbb8
שני שירה < @J10>
2022-12-05 18:32:29 +0200

    pl

---
+++ stage1/PL/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using BlApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IBl bl;
+
+        public MainWindow()
+        {
+            bl= new BlImplementation.Bl();
+            InitializeComponent();
+        }
+
+        private void BTN_Click(object sender, RoutedEventArgs e) => new ProductListWindow(bl).Show();
+        
+    }
+}
---
+++ stage1/PL/ProductListWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using BlApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for ProductListWindow.xaml
+    /// </summary>
+    public partial class ProductListWindow : Window
+    {
+        private IBl bl;
+        public ProductListWindow(IBl Bl )
+        {
+          
+            InitializeComponent();
+            bl = Bl;
+            ProductsListview.ItemsSource = bl.iProduct.ReadAll();
+        }
+
+        private void AttributeSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ProductsListview_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           
+        }
+    }
+}
