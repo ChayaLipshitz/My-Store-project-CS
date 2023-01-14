e2939806388be4d35cd1627e1f056ede4cc2983d
shira <shirass321@gmail.com>
2023-01-14 21:21:49 +0200

    orderTracking

--- stage1/PL/MainWindow.xaml.cs
+++ stage1/PL/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using BlApi;
+using BO;
+using System;
 using System.Windows;
 
 namespace PL;
@@ -20,6 +22,28 @@ public partial class MainWindow : Window
     {
         new ProductListWindow(bl).Show();
         this.Close();
+    }
+
+    private void TrackBTN_Click(object sender, RoutedEventArgs e)
+    {
+        int id=0;
+        try
+        {
+            id = OrderNumTXT.Text==""? throw new Exception("please enter order id!"): Convert.ToInt32(OrderNumTXT.Text);
+            BO.OrderTracking orderTracking = bl.iOrder.Tracking(id);
+            new OrderTracking(bl, orderTracking).Show();
+            this.Close();
+        }
+        catch(NotExistExceptions ex)
+        {
+            MessageBox.Show($"there is no order with id {id}");
+        }
+        catch(Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+
+        }
+
     }
 
     /// <summary>
---
+++ stage1/PL/OrderTracking.xaml.cs
@@ -0,0 +1,49 @@
+﻿using BlApi;
+using BO;
+using Dal.DO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for OrderTracking.xaml
+    /// </summary>
+    public partial class OrderTracking : Window
+    {
+        BO.OrderTracking orderTracking;
+        int orderID;
+        public IBl bl { get; set; }
+        public OrderTracking(IBl BL, BO.OrderTracking orderTracking)
+        {
+            InitializeComponent();
+            bl= BL;
+            try
+            {
+                orderID = orderTracking.ID;
+
+                orderIDLBL.Content = orderID;
+                OrderStatusLBL.Content = OrderStatusLBL.ToString();
+            }
+            catch { 
+            }
+        }
+
+        private void OrderDetailsBTN_Click(object sender, RoutedEventArgs e)
+        {
+            new OrderWindow(bl, orderID).Show();
+            this.Close();
+        }
+    }
+}
