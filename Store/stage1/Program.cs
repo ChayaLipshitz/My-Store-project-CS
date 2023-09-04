15521e08d11dd0f12c9075c426dd8b774c31e1e9
ChayaLipshitz <chl4165772@gmail.com>
2023-09-04 13:21:27 +0300

    my store

--- Store/stage1/Program.cs
+++ Store/stage1/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Worldfdjnhgdxfjnghfjn!");
+Console.WriteLine("Hello, Worldfdjnhgdxfjnghgfdsgfdgfgdfgfdgfgffjn!");
 Console.WriteLine(Console.Read());
